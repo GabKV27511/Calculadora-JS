9e14691afb4e5f6e2cb6723f53d6c1f7c71dc556
GabrielKV <bielkunrath@gmail.com>
2026-07-20 20:03:59 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let valorVisor = "0";
+let numero = document.querySelector("no");
+
+numero.addEventListener ("click", function(){
+    if (valorVisor === 0) {
+        valorVisor = no;
+          console.log(valorVisor)
+    }
+});
+
