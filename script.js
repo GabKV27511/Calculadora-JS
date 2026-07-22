42f56ab29bf0143862b1329061d5947f7d81fd7e
GabrielKV <bielkunrath@gmail.com>
2026-07-22 17:56:22 -0300

    operadores adds

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let valorVisor = "0";
-let numero = document.querySelector("no");
+//parte de colocar os numeros
 
-numero.addEventListener ("click", function(){
-    if (valorVisor === 0) {
-        valorVisor = no;
-          console.log(valorVisor)
-    }
+let visor = document.querySelector("#visorResultados");
+let botoes = document.querySelectorAll(".numeros");
+
+
+botoes.forEach(function(numero){
+    numero.addEventListener("click", function(){
+        if (visor.innerText === "0") {
+            visor.innerText = numero.innerText;
+        } else {
+            visor.innerText += numero.innerText;
+        };
+    });
 });
 
+// parte dos operadores
+
+operadores = document.querySelectorAll(".operadores");
+
+operadores.forEach(function(operador){
+    
+
+    operador.addEventListener("click", function(){
+        let listaDeSinais = "+-";
+        let ultimoCaractere = visor.innerText.slice(-1);
+
+        if (visor.innerText !== "" && !listaDeSinais.includes(ultimoCaractere)){
+            visor.innerText += operador.innerText;
+        }
+    });
+});
